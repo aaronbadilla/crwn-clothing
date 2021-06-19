09c513e08b1569307ecd7ae7681467ca3436bee0
Aaron Badilla <aaronbadilla@Aarons-MacBook-Pro.local>
2021-06-19 12:34:10 -0400

    created cart-item component and added items to dropdown menu. Fixed bug in cartReducer.

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -10,16 +10,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
-                hidden: !state.hidden
-            }
+                ...state, hidden: !state.hidden
+            };
         case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
-                cartItems: addItemToCart(...state.cartItems, action.payload)
-            }
+                cartItems: addItemToCart(state.cartItems, action.payload),
+            };
         default:
             return state;
     }
-}
+};
 
 export default cartReducer
